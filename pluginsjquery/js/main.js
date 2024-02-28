a1d6fa4c143293789a93a162bdf3846aa10d0cd5
Matheus Keske <137569063+MatheusKeske@users.noreply.github.com>
2024-02-28 09:58:44 -0300

    Exercicio

---
+++ pluginsjquery/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // Máscara para telefone
+  $('#telefone').mask('(00) 0000-0000');
+  
+  // Máscara para CPF
+  $('#cpf').mask('000.000.000-00');
+  
+  // Máscara para CEP
+  $('#cep').mask('00000-000');
+});
